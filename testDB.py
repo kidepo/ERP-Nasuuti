4fd25d44bf7b3dceaedcbe68c5e4cdfd258435c3
ogwangmarkp <ogwangmpal@gmail.com>
2017-08-25 15:15:41 -0700

    db connection

---
+++ testDB.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 25 13:05:33 2017
+
+@author: SourceQode
+"""
+
+import DBconnect
+myConnection =DBconnect.getConnection()
+sqlString="SELECT VERSION()"
+if(myConnection):
+ data = DBconnect.fetchData(sqlString)
+ print ("Database version : %s " % data.fetchone())
+else:
+    print("not connected")
+#DBconnect.insertData(myConnection,sqlString)
